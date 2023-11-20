aa5d217d657631b299d6f2d53b8fac0e65d5d8e8
Zhuo Yang <zhuo.yang@twofour-mbp14-zhuoyang.fritz.box>
2023-11-20 17:01:24 +0100

    update gitignore

---
+++ cms/classes/Config.php
@@ -0,0 +1,8 @@
+<?php
+
+class Config {
+    const SMTP_host = 'sandbox.smtp.mailtrap.io';
+    const SMTP_port = 465;
+    const SMTP_user = '3482254d7ef69c';
+    const SMTP_pass = 'ec3117a52c3ba7';
+}
